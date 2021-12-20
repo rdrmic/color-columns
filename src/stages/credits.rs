c0836cc6139060cc5ce91bb53584c8468efd299b
Rade Drmic <rdrmic@gmail.com>
2021-12-20 23:55:44 +0100

    closed #3

--- src/stages/credits.rs
+++ src/stages/credits.rs
@@ -1,13 +1,13 @@
 use ggez::{
-    graphics::{self, DrawParam, PxScale, Text, TextFragment},
+    graphics::{self, Align, DrawParam, PxScale, Text, TextFragment},
     Context,
 };
 use glam::Vec2;
 
 use crate::{
     config::{
-        COLOR_GREEN, CREDITS_TEXT_POSITION, GO_BACK_LABEL_POSITION,
-        HOWTOPLAY_AND_CREDITS_CHAR_SCALE,
+        COLOR_LIGHT_GRAY, CREDITS_CHAR_SCALE, CREDITS_TEXT_POSITION, GO_BACK_LABEL_POSITION,
+        HOWTOPLAY_AND_CREDITS_AREA_WIDTH,
     },
     input::InputEvent,
     resources::Resources,
@@ -24,31 +24,33 @@ impl Credits {
     pub fn new(resources: &Resources) -> Self {
         let font = resources.get_fonts().get_semi_bold();
 
-        // FIXME
-        let controls_text = "
-This game is a remake of
-various old, 'classic',
-columns-like games.
-And it's made in an attempt to
-learn Rust programming
-language.
-
-
-rdrmic@gmail.com
-( any feedback is welcome :)
+        let text_str = "\n\
+            This game is a remake of\n\
+            various old, 'classic',\n\
+            columns-like games.\n\
+            And it's made in an attempt\n\
+            to learn Rust programming\n\
+            language.\n\n\n\
+            rdrmic@gmail.com\n\
+            ( any feedback is welcome :)
         ";
+        let mut text = Text::new(TextFragment {
+            text: text_str.to_string(),
+            color: Some(COLOR_LIGHT_GRAY),
+            font: Some(font),
+            scale: Some(PxScale::from(CREDITS_CHAR_SCALE)),
+        });
+        text.set_bounds(
+            Vec2::new(HOWTOPLAY_AND_CREDITS_AREA_WIDTH, f32::INFINITY),
+            Align::Left,
+        );
 
         Credits {
             go_back_instruction: resources
                 .get_navigation_instructions()
                 .get_go_back()
                 .to_owned(),
-            text: Text::new(TextFragment {
-                text: controls_text.to_string(),
-                color: Some(COLOR_GREEN),
-                font: Some(font),
-                scale: Some(PxScale::from(HOWTOPLAY_AND_CREDITS_CHAR_SCALE)),
-            }),
+            text,
         }
     }
 }
--- src/stages/how_to_play.rs
+++ src/stages/how_to_play.rs
@@ -1,13 +1,15 @@
 use ggez::{
-    graphics::{self, DrawParam, PxScale, Text, TextFragment},
+    graphics::{self, Align, DrawParam, PxScale, Text, TextFragment},
     Context,
 };
 use glam::Vec2;
 
 use crate::{
     config::{
-        COLOR_GREEN, COLOR_RED, GO_BACK_LABEL_POSITION, HOWTOPLAY_AND_CREDITS_CHAR_SCALE,
-        HOWTOPLAY_CONTROLS_TEXT_POSITION, HOWTOPLAY_SCORING_RULES_TEXT_POSITION,
+        COLOR_GREEN, COLOR_RED, GO_BACK_LABEL_POSITION, HOWTOPLAY_AND_CREDITS_AREA_WIDTH,
+        HOWTOPLAY_CONTROLS_CHAR_SCALE, HOWTOPLAY_CONTROLS_LEFTSIDE_TEXT_POSITION_X,
+        HOWTOPLAY_CONTROLS_RIGHTSIDE_TEXT_POSITION_X, HOWTOPLAY_CONTROLS_TEXT_POSITION_Y,
+        HOWTOPLAY_SCORING_CHAR_SCALE, HOWTOPLAY_SCORING_RULES_TEXT_POSITION,
     },
     input::InputEvent,
     resources::Resources,
@@ -17,7 +19,8 @@ use super::{Stage, StageTrait};
 
 pub struct HowToPlay {
     go_back_instruction: Text,
-    controls: Text,
+    controls_leftside: Text,
+    controls_rightside: Text,
     scoring_rules: Text,
 }
 
@@ -25,42 +28,71 @@ impl HowToPlay {
     pub fn new(resources: &Resources) -> Self {
         let font = resources.get_fonts().get_semi_bold();
 
-        // FIXME
-        let controls_text = "-- CONTROLS --
-Right                          RIGHT / D
-Left                             LEFT / A
-Shuffle up                  UP / W
-Shuffle down             DOWN / S
-Drop                            SPACE
+        let controls_leftside_str = "\n\
+            Right:\n\
+            Left:\n\
+            Shuffle up:\n\
+            Shuffle down:\n\
+            Drop:
         ";
-        let scoring_rules_text = "-- SCORING RULES --
-You gain points by matching
-same-colored blocks in all 4
-directions.
-The more matched blocks in a
-line - the more points.
-Also, the points are
-multiplicated by number of
-sequential matchings.
+        let mut controls_leftside = Text::new(TextFragment {
+            text: controls_leftside_str.to_string(),
+            color: Some(COLOR_RED),
+            font: Some(font),
+            scale: Some(PxScale::from(HOWTOPLAY_CONTROLS_CHAR_SCALE)),
+        });
+        controls_leftside.set_bounds(
+            Vec2::new(HOWTOPLAY_AND_CREDITS_AREA_WIDTH, f32::INFINITY),
+            Align::Left,
+        );
+
+        let controls_rightside_str = "\n\
+            RIGHT / D\n\
+            LEFT / A\n\
+            UP / W\n\
+            DOWN / S\n\
+            SPACE
         ";
+        let mut controls_rightside = Text::new(TextFragment {
+            text: controls_rightside_str.to_string(),
+            color: Some(COLOR_RED),
+            font: Some(font),
+            scale: Some(PxScale::from(HOWTOPLAY_CONTROLS_CHAR_SCALE)),
+        });
+        controls_rightside.set_bounds(
+            Vec2::new(HOWTOPLAY_AND_CREDITS_AREA_WIDTH, f32::INFINITY),
+            Align::Left,
+        );
+
+        let scoring_rules_str = "\n\
+            Points are gained by matching\n\
+            same-colored blocks in all 4\n\
+            directions.\n\n\
+            The more matched blocks in a\n\
+            line - the more points gained.\n\n\
+            Also, the points are\n\
+            multiplicated by the number of\n\
+            sequential cascading matchings.
+        ";
+        let mut scoring_rules = Text::new(TextFragment {
+            text: scoring_rules_str.to_string(),
+            color: Some(COLOR_GREEN),
+            font: Some(font),
+            scale: Some(PxScale::from(HOWTOPLAY_SCORING_CHAR_SCALE)),
+        });
+        scoring_rules.set_bounds(
+            Vec2::new(HOWTOPLAY_AND_CREDITS_AREA_WIDTH, f32::INFINITY),
+            Align::Left,
+        );
 
         HowToPlay {
             go_back_instruction: resources
                 .get_navigation_instructions()
                 .get_go_back()
                 .to_owned(),
-            controls: Text::new(TextFragment {
-                text: controls_text.to_string(),
-                color: Some(COLOR_RED),
-                font: Some(font),
-                scale: Some(PxScale::from(HOWTOPLAY_AND_CREDITS_CHAR_SCALE)),
-            }),
-            scoring_rules: Text::new(TextFragment {
-                text: scoring_rules_text.to_string(),
-                color: Some(COLOR_GREEN),
-                font: Some(font),
-                scale: Some(PxScale::from(HOWTOPLAY_AND_CREDITS_CHAR_SCALE)),
-            }),
+            controls_leftside,
+            controls_rightside,
+            scoring_rules,
         }
     }
 }
@@ -83,10 +115,19 @@ impl StageTrait for HowToPlay {
         );
         graphics::queue_text(
             ctx,
-            &self.controls,
+            &self.controls_leftside,
             Vec2::new(
-                HOWTOPLAY_CONTROLS_TEXT_POSITION.0,
-                HOWTOPLAY_CONTROLS_TEXT_POSITION.1,
+                HOWTOPLAY_CONTROLS_LEFTSIDE_TEXT_POSITION_X,
+                HOWTOPLAY_CONTROLS_TEXT_POSITION_Y,
+            ),
+            None,
+        );
+        graphics::queue_text(
+            ctx,
+            &self.controls_rightside,
+            Vec2::new(
+                HOWTOPLAY_CONTROLS_RIGHTSIDE_TEXT_POSITION_X,
+                HOWTOPLAY_CONTROLS_TEXT_POSITION_Y,
             ),
             None,
         );
