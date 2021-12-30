6bbeee7b2aed869608778dcc87b41028f0baf076
Rade Drmic <rdrmic@gmail.com>
2021-12-30 03:45:43 +0100

    Accelerating gameplay gradually: on-screen info for the speedups and max speed reached // #12

--- src/stages/playing/hud.rs
+++ src/stages/playing/hud.rs
@@ -7,11 +7,12 @@ use ggez::{
 
 use crate::{
     config::{
-        COLOR_BLUE, COLOR_GREEN, COLOR_LIGHT_GRAY, COLOR_RED, COLOR_YELLOW, HUD_HIGHSCORE_POSITION,
-        HUD_LABEL_HIGHSCORE_POSITION, HUD_LABEL_INSTRUCTIONS_POSITION, HUD_LABEL_MAXCOMBO_POSITION,
-        HUD_LABEL_PLAYING_STATE_CHAR_SCALE, HUD_LABEL_PLAYING_STATE_POSITION,
-        HUD_LABEL_SCORE_POSITION, HUD_LABEL_SCORING_CHAR_SCALE, HUD_MAXCOMBO_POSITION,
-        HUD_SCORE_POSITION, HUD_SCORING_CHAR_SCALE, NUM_TICKS_FOR_PLAYING_STATE_GO_BLINKING,
+        COLOR_BLUE, COLOR_CYAN, COLOR_GREEN, COLOR_LIGHT_GRAY, COLOR_RED, COLOR_YELLOW,
+        HUD_HIGHSCORE_POSITION, HUD_LABEL_HIGHSCORE_POSITION, HUD_LABEL_INSTRUCTIONS_POSITION,
+        HUD_LABEL_MAXCOMBO_POSITION, HUD_LABEL_PLAYING_STATE_CHAR_SCALE,
+        HUD_LABEL_PLAYING_STATE_POSITION, HUD_LABEL_SCORE_POSITION, HUD_LABEL_SCORING_CHAR_SCALE,
+        HUD_MAXCOMBO_POSITION, HUD_SCORE_POSITION, HUD_SCORING_CHAR_SCALE,
+        NUM_TICKS_FOR_PLAYING_STATE_GO_BLINKING, NUM_TICKS_FOR_PLAYING_STATE_SPEEDUP_BLINKING,
     },
     fonts::Fonts,
     resources::Resources,
@@ -25,6 +26,8 @@ use super::scoring::Scoring;
 struct HudLabels {
     game_info_playing_state_ready: Text,
     game_info_playing_state_go: Text,
+    game_info_playing_state_speedup: Text,
+    game_info_playing_state_maxspeed: Text,
     game_info_playing_state_pause: Text,
     game_info_playing_state_gameover: Text,
     game_info_instructions_ready: Text,
@@ -55,6 +58,16 @@ impl HudLabels {
                 font_extra_bold,
                 "Go!!!",
                 Color::BLACK,
+            ),
+            game_info_playing_state_speedup: Self::create_playing_state_label(
+                font_extra_bold,
+                "Speedup!",
+                COLOR_BLUE,
+            ),
+            game_info_playing_state_maxspeed: Self::create_playing_state_label(
+                font_extra_bold,
+                "Max speed!",
+                COLOR_CYAN,
             ),
             game_info_playing_state_pause: Self::create_playing_state_label(
                 font_extra_bold,
@@ -104,10 +117,11 @@ impl HudLabels {
 *******************************************************************************/
 pub struct Hud {
     labels: HudLabels,
-    pub game_info: Option<GameInfo>,
 
+    pub game_info: Option<GameInfo>,
     num_frames: usize,
     num_blinks: u8,
+    pub maxspeed_reached: bool,
 
     scoring_values_font: Font,
     scoring_values: HudScoringValues,
@@ -118,10 +132,11 @@ impl Hud {
         let font_semi_bold = resources.get_fonts().get_semi_bold();
         Hud {
             labels: HudLabels::new(resources),
-            game_info: None,
 
+            game_info: None,
             num_frames: 0,
             num_blinks: 0,
+            maxspeed_reached: false,
 
             scoring_values_font: font_semi_bold,
             scoring_values: HudScoringValues::new(font_semi_bold, 0), // FIXME refactor
@@ -131,6 +146,7 @@ impl Hud {
     pub fn new_game(&mut self, highscore: usize) {
         self.num_frames = 0;
         self.num_blinks = 0;
+        self.maxspeed_reached = false;
 
         self.scoring_values = HudScoringValues::new(self.scoring_values_font, highscore);
     }
@@ -138,26 +154,55 @@ impl Hud {
     pub fn set_game_info(&mut self, r#type: GameInfoType) {
         self.game_info = match r#type {
             GameInfoType::Ready => Some(GameInfo {
+                r#type,
                 playing_state: self.labels.game_info_playing_state_ready.clone(),
-                instructions: self.labels.game_info_instructions_ready.clone(),
+                instructions: Some(self.labels.game_info_instructions_ready.clone()),
             }),
             GameInfoType::Go => Some(GameInfo {
+                r#type,
                 playing_state: self.labels.game_info_playing_state_go.clone(),
-                instructions: self.labels.game_info_instructions_go.clone(),
+                instructions: Some(self.labels.game_info_instructions_go.clone()),
+            }),
+            GameInfoType::Speedup => Some(GameInfo {
+                r#type,
+                playing_state: self.labels.game_info_playing_state_speedup.clone(),
+                instructions: None,
+            }),
+            GameInfoType::MaxSpeed => Some(GameInfo {
+                r#type,
+                playing_state: self.labels.game_info_playing_state_maxspeed.clone(),
+                instructions: None,
             }),
             GameInfoType::Pause => Some(GameInfo {
+                r#type,
                 playing_state: self.labels.game_info_playing_state_pause.clone(),
-                instructions: self.labels.game_info_instructions_pause.clone(),
+                instructions: Some(self.labels.game_info_instructions_pause.clone()),
             }),
             GameInfoType::GameOver => Some(GameInfo {
+                r#type,
                 playing_state: self.labels.game_info_playing_state_gameover.clone(),
-                instructions: self.labels.game_info_instructions_gameover.clone(),
+                instructions: Some(self.labels.game_info_instructions_gameover.clone()),
             }),
             GameInfoType::None => None,
+        };
+        if r#type != GameInfoType::Pause {
+            self.num_frames = 0;
+            self.num_blinks = 0;
         }
     }
 
     pub fn update_game_info(&mut self) {
+        if self.game_info.is_some() {
+            match self.game_info.as_ref().unwrap().r#type {
+                GameInfoType::Go => self.update_game_info_go(),
+                GameInfoType::Speedup => self.update_game_info_speedup(),
+                GameInfoType::MaxSpeed => self.update_game_info_maxspeed(),
+                _ => (),
+            }
+        }
+    }
+
+    fn update_game_info_go(&mut self) {
         self.num_frames += 1;
         if self.num_blinks < 3 {
             if self.num_frames % NUM_TICKS_FOR_PLAYING_STATE_GO_BLINKING == 0 {
@@ -175,16 +220,69 @@ impl Hud {
             }
         } else {
             let game_info = self.game_info.as_mut().unwrap();
-            let mut instructions_fragment = &mut game_info.instructions.fragments_mut()[0];
-            if let Some(mut instructions_color) = instructions_fragment.color {
-                instructions_color.a -= 0.001;
-                if instructions_color.a > 0.0 {
-                    instructions_fragment.color = Some(instructions_color);
-                } else {
-                    self.game_info = None;
+            if let Some(game_info_instructions) = &mut game_info.instructions {
+                let mut instructions_fragment = &mut game_info_instructions.fragments_mut()[0];
+                if let Some(mut instructions_color) = instructions_fragment.color {
+                    instructions_color.a -= 0.001;
+                    if instructions_color.a > 0.0 {
+                        instructions_fragment.color = Some(instructions_color);
+                    } else {
+                        self.reset_animation(GameInfoType::None);
+                    }
                 }
             }
         }
+    }
+
+    fn update_game_info_speedup(&mut self) {
+        self.num_frames += 1;
+        if self.num_frames % NUM_TICKS_FOR_PLAYING_STATE_SPEEDUP_BLINKING == 0 {
+            let game_info = self.game_info.as_mut().unwrap();
+            let mut playing_state_fragment = &mut game_info.playing_state.fragments_mut()[0];
+            if let Some(mut playing_state_color) = playing_state_fragment.color {
+                if playing_state_color == COLOR_BLUE {
+                    playing_state_color = Color::BLACK;
+                } else {
+                    playing_state_color = COLOR_BLUE;
+                    self.num_blinks += 1;
+                }
+                if self.num_blinks < 3 {
+                    playing_state_fragment.color = Some(playing_state_color);
+                } else {
+                    let next_game_info;
+                    if self.maxspeed_reached {
+                        next_game_info = GameInfoType::MaxSpeed;
+                    } else {
+                        next_game_info = GameInfoType::None;
+                    }
+                    self.reset_animation(next_game_info);
+                }
+            }
+        }
+    }
+
+    fn update_game_info_maxspeed(&mut self) {
+        self.num_frames += 1;
+        let game_info_playing_state_fragment = &mut self
+            .game_info
+            .as_mut()
+            .unwrap()
+            .playing_state
+            .fragments_mut()[0];
+        if let Some(mut playing_state_color) = game_info_playing_state_fragment.color {
+            playing_state_color.a -= 0.0018;
+            if playing_state_color.a > 0.0 {
+                game_info_playing_state_fragment.color = Some(playing_state_color);
+            } else {
+                self.reset_animation(GameInfoType::None);
+            }
+        }
+    }
+
+    fn reset_animation(&mut self, next_game_info: GameInfoType) {
+        self.num_frames = 0;
+        self.num_blinks = 0;
+        self.set_game_info(next_game_info);
     }
 
     pub fn update_scoring(&mut self, scoring: &Scoring) {
@@ -212,15 +310,17 @@ impl Hud {
                 ),
                 None,
             );
-            graphics::queue_text(
-                ctx,
-                &game_info.instructions,
-                Vec2::new(
-                    HUD_LABEL_INSTRUCTIONS_POSITION.0,
-                    HUD_LABEL_INSTRUCTIONS_POSITION.1,
-                ),
-                None,
-            );
+            if let Some(game_info_instructions) = &game_info.instructions {
+                graphics::queue_text(
+                    ctx,
+                    game_info_instructions,
+                    Vec2::new(
+                        HUD_LABEL_INSTRUCTIONS_POSITION.0,
+                        HUD_LABEL_INSTRUCTIONS_POSITION.1,
+                    ),
+                    None,
+                );
+            }
         }
         // SCORE
         graphics::queue_text(
@@ -275,17 +375,22 @@ impl Hud {
     }
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum GameInfoType {
     None,
     Ready,
     Go,
+    Speedup,
+    MaxSpeed,
     Pause,
     GameOver,
 }
 
+#[derive(Clone)]
 pub struct GameInfo {
+    pub r#type: GameInfoType,
     playing_state: Text,
-    instructions: Text,
+    instructions: Option<Text>,
 }
 
 struct HudScoringValues {
