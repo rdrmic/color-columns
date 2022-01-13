b4a3070cfc2f6b5f03baf5b146c5c7e36df1d32d
Rade Drmic <rdrmic@gmail.com>
2022-01-13 23:54:06 +0100

    Code clean-up

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,7 @@
 #![warn(clippy::pedantic)]
 #![warn(clippy::nursery)]
 #![warn(clippy::cargo)]
-// from clippy::restriction:
-#![warn(clippy::unwrap_used)]
-#![warn(clippy::todo)]
-#![warn(clippy::print_stdout)] // FIXME - BUG in clippy
-
+#![warn(clippy::unwrap_used, clippy::todo, clippy::print_stdout)] // from clippy:restriction
 // CONSOLE-FREE EXECUTABLE ON RELEASE BUILDS
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
